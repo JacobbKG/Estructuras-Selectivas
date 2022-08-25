d2df879b37b21e887d2f04a8e99f29b24a9b31be
Jacobb26 <fmartinezd3@miumg.edu.gt>
2022-08-25 10:20:10 -0600

    Estructuras Selectivas

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using System.Globalization;
+using System;
+
+internal class Program 
+{
+		static void Main(string[] args)  
+        {
+         
+         string memb;
+         int precio;
+         double des1;
+         double des2;
+         double des3;
+         double total1;
+         double total2;
+         double total3;
+
+         Console.WriteLine("Ingrese el precio de su helado: ");
+         precio = Convert.ToInt32(Console.ReadLine());
+
+         Console.WriteLine("Ingrese su tipo de membrecia: ");
+         Console.WriteLine("La membrecia A: ");
+         Console.WriteLine("La membrecia B: ");
+         Console.WriteLine("La membrecia C: ");
+         Console.WriteLine("Si no cuenta con membrecia ingrese la palabra: no ");
+         memb = Console.ReadLine ();
+
+         switch (memb) {
+            case "A":
+				des1 = precio*0.10;
+				Console.WriteLine("Su descuento es de: Q"+des1);
+				total1 = precio-des1;
+				Console.WriteLine("su total a pagar es de: Q"+total1); break;
+			case "B":
+				des2 = precio*0.15;
+				Console.WriteLine("Su descuento es de: Q"+des2);
+				total2 = precio-des2;
+				Console.WriteLine("su total a pagar es de: Q"+total2); break;
+			case "C":
+				des3 = precio*0.20;
+				Console.WriteLine("Su descuento es de: Q"+des3);
+				total3 = precio-des3;
+				Console.WriteLine("su total a pagar es de: Q"+total3); break;
+			default:
+				Console.WriteLine("Su total a pagar es de: Q"+precio); break;
+       }
+         Console.ReadKey ();
+
+    }
+}
+             
