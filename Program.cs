2d681cb5a13f1d09fcd5f37edc08c90b51a1fb0d
Jacobb26 <fmartinezd3@miumg.edu.gt>
2022-08-25 11:18:43 -0600

    modificacion

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ internal class Program
         {
          
          string memb;
+         string res;
          int precio;
          double des1;
          double des2;
@@ -20,34 +21,45 @@ internal class Program
          Console.WriteLine("Ingrese el precio de su helado: ");
          precio = Convert.ToInt32(Console.ReadLine());
 
-         Console.WriteLine("Ingrese su tipo de membrecia: ");
-         Console.WriteLine("La membrecia A: ");
-         Console.WriteLine("La membrecia B: ");
-         Console.WriteLine("La membrecia C: ");
-         Console.WriteLine("Si no cuenta con membrecia ingrese la palabra: no ");
-         memb = Console.ReadLine ();
+         Console.WriteLine("Cuenta con alguna membrecia?");
+			   res = Console.ReadLine();
+
+
+        if (res.Equals("si")) {
+				Console.WriteLine("Ingrese su tipo de membrecia: ");
+				Console.WriteLine("membrecia tipo A su descuento es del 10%: ");
+				Console.WriteLine("membrecia tipo B su descuento es del 15%: ");
+				Console.WriteLine("membrecia tipo C su descuento es del 20%: ");
+				memb = Console.ReadLine();
 
          switch (memb) {
-            case "A":
+
+    case "A":
 				des1 = precio*0.10;
 				Console.WriteLine("Su descuento es de: Q"+des1);
 				total1 = precio-des1;
 				Console.WriteLine("su total a pagar es de: Q"+total1); break;
-			case "B":
+		case "B":
 				des2 = precio*0.15;
 				Console.WriteLine("Su descuento es de: Q"+des2);
 				total2 = precio-des2;
 				Console.WriteLine("su total a pagar es de: Q"+total2); break;
-			case "C":
+		case "C":
 				des3 = precio*0.20;
 				Console.WriteLine("Su descuento es de: Q"+des3);
 				total3 = precio-des3;
 				Console.WriteLine("su total a pagar es de: Q"+total3); break;
 			default:
 				Console.WriteLine("Su total a pagar es de: Q"+precio); break;
-       }
-         Console.ReadKey ();
 
-    }
-}
-             
+         }
+
+         } else {
+				Console.WriteLine("Su total a pagar es de: Q"+precio); 
+         }
+
+
+         }
+      }
+  
+
